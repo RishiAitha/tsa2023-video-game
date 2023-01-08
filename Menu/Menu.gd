4d27851f6f9fd8bbcf887d086371ac5ba5138625
RishiAitha <110874460+RishiAitha@users.noreply.github.com>
2023-01-07 23:12:33 -0500

    menu scene added

---
+++ Menu/Menu.gd
@@ -0,0 +1,42 @@
+extends Node
+
+func _ready():
+	$Start.show()
+	$"2Player".hide()
+	$"3Player".hide()
+	$"4Player".hide()
+	$MainMenu.hide()
+	$ScrollContainer.hide()
+
+func _on_Start_pressed():
+	$Start.hide()
+	$"2Player".show()
+	$"3Player".show()
+	$"4Player".show()
+
+func _on_2Player_pressed():
+	ColorsSingleton.colors = ["Blue", "dead", "Yellow", "dead"]
+	get_tree().change_scene("res://Main/Main.tscn")
+
+func _on_3Player_pressed():
+	ColorsSingleton.colors = ["Blue", "Red", "Yellow", "dead"]
+	get_tree().change_scene("res://Main/Main.tscn")
+
+func _on_4Player_pressed():
+	ColorsSingleton.colors = ["Blue", "Red", "Yellow", "Green"]
+	get_tree().change_scene("res://Main/Main.tscn")
+
+func _on_HowToPlay_pressed():
+	$Start.hide()
+	$"2Player".hide()
+	$"3Player".hide()
+	$"4Player".hide()
+	$HowToPlay.hide()
+	$MainMenu.show()
+	$ScrollContainer.show()
+
+func _on_MainMenu_pressed():
+	$ScrollContainer.hide()
+	$MainMenu.hide()
+	$Start.show()
+	$HowToPlay.show()
